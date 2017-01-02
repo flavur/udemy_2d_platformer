58543b209cc85e1011ecfeb24c0899ce2b23ea97
theonlyoxtale <theonlyoxtale@gmail.com>
2017-01-02 15:18:30 -0500

    Player can now only jump if touching the ground

--- 2D Platformer/Assets/_Code/PlayerController.cs
+++ 2D Platformer/Assets/_Code/PlayerController.cs
@@ -7,6 +7,12 @@ public class PlayerController : MonoBehaviour {
 	public float jumpSpeed = 2f; //Player jump speed
 	private Rigidbody2D myRigidBody; //Player RigidBody
 
+	// Variables used to check whether the player is touching the ground
+	public Transform groundCheck; //Position in space
+	public float groundCheckRadius; //Radius of the groundCheck Position
+	public LayerMask whatIsGround;
+	public bool isGrounded;
+
 	// Use this for initialization
 	void Start () {
 		myRigidBody = GetComponent<Rigidbody2D>();
@@ -14,13 +20,17 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		// Boolean variable that checks whether the player is on the ground or not
+		isGrounded = Physics2D.OverlapCircle(groundCheck.position,groundCheckRadius,whatIsGround);
+
 		// Move the player to the right
 		if (Input.GetAxisRaw("Horizontal") > 0f){myRigidBody.velocity = new Vector3(moveSpeed,myRigidBody.velocity.y,0f);}
 		// Move the player to the left
 		if (Input.GetAxisRaw("Horizontal") < 0f){myRigidBody.velocity = new Vector3(-moveSpeed,myRigidBody.velocity.y,0f);}
 		// Player shouldn't be moving
 		if (Input.GetAxisRaw("Horizontal") == 0f){myRigidBody.velocity = new Vector3(0f,myRigidBody.velocity.y,0f);}
-		// Make the player jump
-		if (Input.GetButtonDown("Jump")){myRigidBody.velocity = new Vector3(myRigidBody.velocity.x,jumpSpeed,0f);}
+		// Make the player jump (as long as the player is on the ground)
+		if (Input.GetButtonDown("Jump") && isGrounded){myRigidBody.velocity = new Vector3(myRigidBody.velocity.x,jumpSpeed,0f);}
 	}
 }
