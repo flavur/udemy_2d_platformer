d026798541569293c4927f4ec4865eba4b45e930
theonlyoxtale <theonlyoxtale@gmail.com>
2017-01-02 13:08:08 -0500

    created initial C# script
    Created PlayerController script and added code for finding the player's
    rigidbody component and created variable for player moveSpeed

---
+++ 2D Platformer/Assets/_Code/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed; //player move speed
+	private Rigidbody2D myRigidBody;
+
+	// Use this for initialization
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
