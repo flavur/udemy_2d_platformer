90cc9aa09f30eb06f3b1deec8ee487305bafc0cd
theonlyoxtale <theonlyoxtale@gmail.com>
2017-01-05 20:55:20 -0500

    added a coroutine to add a timer delay to respawn

--- 2D Platformer/Assets/_Code/LevelManager.cs
+++ 2D Platformer/Assets/_Code/LevelManager.cs
@@ -13,12 +13,20 @@ public class LevelManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
 	//deactivates the player moves them and then reactivates them
 	public void Respawn(){
+		StartCoroutine("RespawnCoRoutine");
+	}
+
+	public IEnumerator RespawnCoRoutine(){
 		thePlayer.gameObject.SetActive(false);
+
+		//tells game to wait for a certain amount of time before resuming
+		yield return new WaitForSeconds(waitToRespawn);
+
 		thePlayer.transform.position = thePlayer.respawnPosition;
 		thePlayer.gameObject.SetActive(true);
 	}
