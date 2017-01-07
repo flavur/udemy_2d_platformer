0cf0abe4f26e5220788861ee84119480a4fca23e
theonlyoxtale <theonlyoxtale@gmail.com>
2017-01-07 13:06:20 -0500

    added script to destroy used gameobjects overtime

---
+++ 2D Platformer/Assets/_Code/DestroyOverTime.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOverTime : MonoBehaviour {
+
+	public float lifeTime;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//taking a fraction of the lifetime away until lifetime == 0
+		lifeTime = lifeTime - Time.deltaTime;
+		//if lifeTime is less than or equal to zero then Destroy the gameObject this script is attached to
+		if (lifeTime <= 0f)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
