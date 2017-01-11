9028d0df66aeb58fa76cfe2cf2d04b4ededfbdc9
theonlyoxtale <theonlyoxtale@gmail.com>
2017-01-10 20:25:36 -0500

    added a patrolling enemy and fixed the other enemy
    Fixed EnemyOne sticking to walls by adding another material to the other
    collider and did the same for EnemyTwo

---
+++ 2D Platformer/Assets/_Code/EnemyTwoController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyTwoController : MonoBehaviour {
+	public Transform leftPoint;
+	public Transform rightPoint;
+	public float moveSpeed;
+	public bool movingRight;
+
+	private Rigidbody2D myRigidbody;
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//which direction is the enemy moving towards
+		if (movingRight && transform.position.x > rightPoint.position.x)
+		{
+			movingRight = false;
+		}
+		if (!movingRight && transform.position.x < leftPoint.position.x)
+		{
+			movingRight = true;
+		}
+
+		//Once we know which direction the enemy is moving then
+		//if player is moving right then move enemy to the right
+		if (movingRight)
+		{
+			myRigidbody.velocity = new Vector3(moveSpeed,myRigidbody.velocity.y,0f);
+		}
+		//if enemy is not moving to the right then move enemy to the left
+		else
+		{
+			myRigidbody.velocity = new Vector3(-moveSpeed,myRigidbody.velocity.y,0f);
+		}
+	}
+}
