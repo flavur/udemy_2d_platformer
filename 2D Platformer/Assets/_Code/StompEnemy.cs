d48bd546efa5a79e7accf8a9e7a22d98b521f085
theonlyoxtale <theonlyoxtale@gmail.com>
2017-01-11 21:40:45 -0500

    created script that kills enemy when you jump on it

---
+++ 2D Platformer/Assets/_Code/StompEnemy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StompEnemy : MonoBehaviour {
+
+	public GameObject deathSplosion;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/// <summary>
+	/// Sent when another object enters a trigger collider attached to this
+	/// object (2D physics only).
+	/// </summary>
+	/// <param name="other">The other Collider2D involved in this collision.</param>
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Enemy")
+		{
+			Destroy(other.gameObject);
+			Instantiate(deathSplosion,other.transform.position,other.transform.rotation);
+		}
+	}
+}
