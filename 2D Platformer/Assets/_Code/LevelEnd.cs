1ab1202d512f47709d6373f9d1cdf0261ce2c47a
theonlyoxtale <theonlyoxtale@gmail.com>
2017-01-30 21:09:35 -0500

    added the ability to load levels based on a trigger

---
+++ 2D Platformer/Assets/_Code/LevelEnd.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEnd : MonoBehaviour {
+
+	public string levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/// <summary>
+	/// Sent when another object enters a trigger collider attached to this
+	/// object (2D physics only).
+	/// </summary>
+	/// <param name="other">The other Collider2D involved in this collision.</param>
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			SceneManager.LoadScene(levelToLoad);
+		}
+	}
+
+}
