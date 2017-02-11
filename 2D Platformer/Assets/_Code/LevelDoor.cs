72b05f34c0785a6e01376df51fd3c5061ac0d7e2
theonlyoxtale <theonlyoxtale@gmail.com>
2017-02-11 01:11:10 -0500

    added ability to select different levels

---
+++ 2D Platformer/Assets/_Code/LevelDoor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelDoor : MonoBehaviour {
+
+	public string levelToLoad;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/// <summary>
+	/// Sent each frame where another object is within a trigger collider
+	/// attached to this object (2D physics only).
+	/// </summary>
+	/// <param name="other">The other Collider2D involved in this collision.</param>
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			if (Input.GetButtonDown("Jump"))
+			{
+				SceneManager.LoadScene(levelToLoad);
+			}
+		}
+	}
+}
