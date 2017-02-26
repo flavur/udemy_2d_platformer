ef1e38b988b4d6a73ffb35c2cb608ed07d192b48
theonlyoxtale <theonlyoxtale@gmail.com>
2017-02-26 10:34:03 -0500

    finalized the pause menu

--- 2D Platformer/Assets/_Code/PauseScreen.cs
+++ 2D Platformer/Assets/_Code/PauseScreen.cs
@@ -8,40 +8,59 @@ public class PauseScreen : MonoBehaviour
 
     public string levelSelect;
     public string mainMenu;
-	public GameObject thePauseScreen;
+    public GameObject thePauseScreen;
 
-	private LevelManager theLevelManager;
+    private LevelManager theLevelManager;
+    private PlayerController thePlayer;
 
     // Use this for initialization
     void Start()
     {
-		theLevelManager = FindObjectOfType<LevelManager>();
+        theLevelManager = FindObjectOfType<LevelManager>();
+        thePlayer = FindObjectOfType<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetKeyDown(KeyCode.P))
-		{
-			thePauseScreen.SetActive(true);
-		}
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+			if (Time.timeScale == 0f)
+			{
+				ResumeGame();
+			}
+			else
+			{
+				PauseGame();
+			}
+        }
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        thePauseScreen.SetActive(true);
+        thePlayer.canMove = false;
+        theLevelManager.levelMusic.Pause();
     }
 
     public void ResumeGame()
     {
-		thePauseScreen.SetActive(false);
+        thePauseScreen.SetActive(false);
+        Time.timeScale = 1;
+        thePlayer.canMove = true;
+        theLevelManager.levelMusic.Play();
     }
 
     public void LevelSelect()
     {
-		PlayerPrefs.SetInt("PlayerLives",theLevelManager.currentLives);
-		PlayerPrefs.SetInt("GemCount",theLevelManager.gemCount);
-
-		SceneManager.LoadScene(levelSelect);
+        PlayerPrefs.SetInt("PlayerLives", theLevelManager.currentLives);
+        PlayerPrefs.SetInt("GemCount", theLevelManager.gemCount);
+        SceneManager.LoadScene(levelSelect);
     }
 
     public void QuitToMain()
     {
-		SceneManager.LoadScene(mainMenu);
+        SceneManager.LoadScene(mainMenu);
     }
 }
