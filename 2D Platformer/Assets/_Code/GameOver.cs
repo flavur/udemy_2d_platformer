b95d2dd641dc3af5bce5116843f624742ecf5643
theonlyoxtale <theonlyoxtale@gmail.com>
2017-02-25 20:14:55 -0500

    added menu for game over screen
    fixed some rendering issues, added code to create working game over menu
    screen and added  updated positioning of some game elements so they work
    properly

---
+++ 2D Platformer/Assets/_Code/GameOver.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+
+    public string mainMenu;
+    public string levelSelect;
+
+    private LevelManager theLevelManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        theLevelManager = FindObjectOfType<LevelManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Restart()
+    {
+        PlayerPrefs.SetInt("GemCount", 0);
+        PlayerPrefs.SetInt("PlayerLives", theLevelManager.startingLives);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LevelSelect()
+    {
+        PlayerPrefs.SetInt("GemCount", 0);
+        PlayerPrefs.SetInt("PlayerLives", theLevelManager.startingLives);
+
+		SceneManager.LoadScene(levelSelect);
+    }
+
+    public void QuitToMainMenu()
+    {
+        SceneManager.LoadScene(mainMenu);
+    }
+}
