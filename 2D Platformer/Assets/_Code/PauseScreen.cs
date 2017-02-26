7b0a7b6d02b3e5f30c9224b4082009322d7780dd
theonlyoxtale <theonlyoxtale@gmail.com>
2017-02-25 21:37:12 -0500

    Started to create the pause menu

---
+++ 2D Platformer/Assets/_Code/PauseScreen.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScreen : MonoBehaviour
+{
+
+    public string levelSelect;
+    public string mainMenu;
+	public GameObject thePauseScreen;
+
+	private LevelManager theLevelManager;
+
+    // Use this for initialization
+    void Start()
+    {
+		theLevelManager = FindObjectOfType<LevelManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			thePauseScreen.SetActive(true);
+		}
+    }
+
+    public void ResumeGame()
+    {
+		thePauseScreen.SetActive(false);
+    }
+
+    public void LevelSelect()
+    {
+		PlayerPrefs.SetInt("PlayerLives",theLevelManager.currentLives);
+		PlayerPrefs.SetInt("GemCount",theLevelManager.gemCount);
+
+		SceneManager.LoadScene(levelSelect);
+    }
+
+    public void QuitToMain()
+    {
+		SceneManager.LoadScene(mainMenu);
+    }
+}
